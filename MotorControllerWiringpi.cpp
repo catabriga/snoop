f1eba221274403eae3385cbc050b648e9edade6e
Lucas Catabriga <catabriga90@gmail.com>
2017-09-09 18:27:46 -0300

    Wiring pi motor controller

--- MotorControllerWiringpi.cpp
+++ MotorControllerWiringpi.cpp
@@ -26,17 +26,17 @@ MotorControllerWiringpi::MotorControllerWiringpi(void)
     pinMode(MOTOR_A_IN1_PIN, OUTPUT);
     pinMode(MOTOR_A_IN2_PIN, OUTPUT);
 
-    //pinMode(MOTOR_B_ENABLE_PIN, OUTPUT);
-    //pinMode(MOTOR_B_IN1_PIN, OUTPUT);
-    //pinMode(MOTOR_B_IN2_PIN, OUTPUT);
+    pinMode(MOTOR_B_ENABLE_PIN, OUTPUT);
+    pinMode(MOTOR_B_IN1_PIN, OUTPUT);
+    pinMode(MOTOR_B_IN2_PIN, OUTPUT);
 
     digitalWrite(MOTOR_A_ENABLE_PIN, LOW);
     digitalWrite(MOTOR_A_IN1_PIN, HIGH);
     digitalWrite(MOTOR_A_IN2_PIN, LOW);
 
-    //digitalWrite(MOTOR_B_ENABLE_PIN, LOW);
-    //digitalWrite(MOTOR_B_IN1_PIN, HIGH);
-    //digitalWrite(MOTOR_B_IN2_PIN, LOW);
+    digitalWrite(MOTOR_B_ENABLE_PIN, LOW);
+    digitalWrite(MOTOR_B_IN1_PIN, HIGH);
+    digitalWrite(MOTOR_B_IN2_PIN, LOW);
 }
 
 void MotorControllerWiringpi::setMotorA(int8_t power)
@@ -62,17 +62,17 @@ void MotorControllerWiringpi::setMotorB(int8_t power)
 {
     if(power < 0)
     {
-        //digitalWrite(MOTOR_B_IN1_PIN, HIGH);
-        //digitalWrite(MOTOR_B_IN2_PIN, LOW);
+        digitalWrite(MOTOR_B_IN1_PIN, HIGH);
+        digitalWrite(MOTOR_B_IN2_PIN, LOW);
     }
     else
     {
-        //digitalWrite(MOTOR_B_IN1_PIN, LOW);
-        //digitalWrite(MOTOR_B_IN2_PIN, HIGH);
+        digitalWrite(MOTOR_B_IN1_PIN, LOW);
+        digitalWrite(MOTOR_B_IN2_PIN, HIGH);
     }
 
     // wiringpi uses a 1024 range for its pwm
     // so our 128 range must be multiplied by 8
     int32_t pwmPower = 8 * abs(power);
-    //pwmWrite(MOTOR_B_ENABLE_PIN, pwmPower);
+    pwmWrite(MOTOR_B_ENABLE_PIN, pwmPower);
 }
